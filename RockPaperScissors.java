36bb13c64f6dca9bdba86e6f48f913f12957f20b
Noah (Pham Tran Khoi Nguyen) <ptknguyen04@gmail.com>
2024-05-31 18:14:49 +0100

    simple way to repeat the game multiple time and some comments

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,58 +1,71 @@
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * The RockPaperScissors class is a simple implementation of the Rock, Paper, Scissors game.
+ * It allows the user to play against the computer and keeps track of the score.
+ */
 public class RockPaperScissors {
     public static void main(String[] args) {
-        String[] rps = {"r", "p", "s"};
-        String computerMove = rps[new Random().nextInt(rps.length)];
 
         Scanner scanner = new Scanner(System.in);
-        String playerMove;
-        
+
         while(true) {
-            System.out.println("Please enter your move (r, p or s)");
-            playerMove = scanner.nextLine();
-            if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
+            String[] rps = {"r", "p", "s"};
+            String computerMove = rps[new Random().nextInt(rps.length)];
+
+            String playerMove;
+            
+            // Prompt the user to enter their move
+            while(true) {
+                System.out.println("Please enter your move (r, p or s)");
+                playerMove = scanner.nextLine();
+                if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
+                    break;
+                }
+                System.out.println("Player move is not valid! Please enter another move.");
+            }
+
+            System.out.println("Computer played: " + computerMove);
+
+            //statements to check the move between the computer and the player
+            if (playerMove.equals(computerMove)) {
+                System.out.println("The game is a tie.");
+            }
+            else if (playerMove.equals("r")) {
+                if (computerMove.equals("s")) {
+                    System.out.println("You Win!");
+                }
+                if (computerMove.equals("p")) {
+                    System.out.println("You Lose!");
+                }
+            }
+
+            else if (playerMove.equals("p")) {
+                if (computerMove.equals("s")) {
+                    System.out.println("You Lose!");
+                }
+                if (computerMove.equals("r")) {
+                    System.out.println("You Win!");
+                }
+            }
+
+            else if (playerMove.equals("s")) {
+                if (computerMove.equals("p")) {
+                    System.out.println("You Win!");
+                }
+                if (computerMove.equals("r")) {
+                    System.out.println("You Lose!");
+                }
+            }
+            System.out.println("Play again? (y or n)");
+            String playAgain = scanner.nextLine();
+
+            if (!playAgain.equals("y")) {
                 break;
             }
-            System.out.println("Player move is not valid! Please enter another move.");
         }
-
-        
-
-        System.out.println("Computer played: " + computerMove);
-
-        if (playerMove.equals(computerMove)) {
-            System.out.println("The game is a tie.");
-        }
-        else if (playerMove.equals("r")) {
-            if (computerMove.equals("s")) {
-                System.out.println("You Win!");
-            }
-            if (computerMove.equals("p")) {
-                System.out.println("You Lose!");
-            }
-        }
-
-        else if (playerMove.equals("p")) {
-            if (computerMove.equals("s")) {
-                System.out.println("You Lose!");
-            }
-            if (computerMove.equals("r")) {
-                System.out.println("You Win!");
-            }
-        }
-
-        else if (playerMove.equals("s")) {
-            if (computerMove.equals("p")) {
-                System.out.println("You Win!");
-            }
-            if (computerMove.equals("r")) {
-                System.out.println("You Lose!");
-            }
-        }
-
-
+        scanner.close(); //good prgrammer closes their scanner :)
 
     }
 }
